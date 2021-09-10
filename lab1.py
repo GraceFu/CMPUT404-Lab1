0cd550f51d110e62720b26a200fda24e0529b013
GraceFu <46909395+GraceFu@users.noreply.github.com>
2021-09-11 02:53:41 +0800

    edit

--- lab1.py
+++ lab1.py
@@ -4,4 +4,12 @@ import requests
 print(requests.__version__)
 
 # Modify your Python script to GET the Google homepage.
-output = requests.get("https://www.baidu.com/")
+output = requests.get("https://www.google.com/")
+
+# Modify your Python script so that it downloads itself from GitHub and prints out its own source code from GitHub.
+from os import getcwd
+
+raw_file = requests.get("https://raw.githubusercontent.com/GraceFu/CMPUT404-Lab1/master/lab1.py")
+filename = getcwd() + "lab1.txt"
+f = open(filename,'w')
+f.write(raw_file.text)
